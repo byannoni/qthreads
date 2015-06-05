d4ab157067ced12ed58fd576edfc1da01576eea1
Brandon Yannoni <byannoni+github@gmail.com>
2015-06-05 14:53:35 -0400

    Resolves #12 - general clean up

--- function_queue.h
+++ function_queue.h
@@ -10,12 +10,12 @@ struct function_queue_element {
 };
 
 struct function_queue {
-	unsigned front;
-	unsigned back;
-	unsigned max_elements;
-	unsigned size;
 	struct function_queue_element* elements;
 	pthread_mutex_t lock;
+	unsigned int front;
+	unsigned int back;
+	unsigned int max_elements;
+	unsigned int size;
 };
 
 /*
@@ -33,13 +33,13 @@ enum {
 extern "C" {
 #endif
 
-int fq_init( struct function_queue*, unsigned );
-int fq_destroy( struct function_queue* );
-int fq_push( struct function_queue*, struct function_queue_element, int );
-int fq_pop( struct function_queue*, struct function_queue_element*, int );
-int fq_peek( struct function_queue*, struct function_queue_element*, int );
-int fq_is_empty( struct function_queue*, int );
-int fq_is_full( struct function_queue*, int );
+int fq_init(struct function_queue*, unsigned);
+int fq_destroy(struct function_queue*);
+int fq_push(struct function_queue*, struct function_queue_element, int);
+int fq_pop(struct function_queue*, struct function_queue_element*, int);
+int fq_peek(struct function_queue*, struct function_queue_element*, int);
+int fq_is_empty(struct function_queue*, int);
+int fq_is_full(struct function_queue*, int);
 
 #ifdef __cplusplus
 }
